484e02094bc5ef3c7d1b04ce34bef0949ac8e59d
dxn <1684193123@qq.com>
2023-06-30 23:00:34 +0800

    1

--- bot.py
+++ bot.py
@@ -1,10 +1,5 @@
 import nonebot
-# from xiaonan_adapter import Adapter as xiaonan_Adapter
-from xiaonan_adapter import Adapter as Adapter
-
-# from yarl import URL as URL
-# from nonebot import get_driver
-# from nonebot.drivers import ReverseDriver
+from xiaonan_adapter import Adapter as XiaonanAdapter
 
 # 初始化 NoneBot
 nonebot.init()
@@ -12,20 +7,15 @@ app = nonebot.get_asgi()
 
 # 注册适配器
 driver = nonebot.get_driver()
-driver.register_adapter(Adapter)
-
-# can_use = isinstance(get_driver(), ReverseDriver)
-# print("是否为服务端类型" + str(can_use))
+driver.register_adapter(XiaonanAdapter)
 
 # 在这里加载插件
 # nonebot.load_builtin_plugins("echo")  # 内置插件
 # nonebot.load_plugin("thirdparty_plugin")  # 第三方插件
 nonebot.load_plugins("src/plugins") # 本地插件
-# URL.is_absolute(URL("http://127.0.0.1:8888"))
+
 # config = driver.config
 # nonebot.load_all_plugins(set(config.plugins), set(config.plugin_dirs))
-# superusers = config.superusers
-
 
 if __name__ == "__main__":
     nonebot.run(app="__mp_main__:app")
